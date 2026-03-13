ef21dbc378675ae59d74a624b1d9563779f32bbf
DemonDED <DemonDED@inbox.ru>
2026-03-13 12:58:50 +0700

    enable develop branch, create main file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main() {
+	const char *suit[4] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+	const char *face[13] = {"Ace", "Deus", "Three", "Four", "Five", "Six", "Seven",
+									"Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+	int deck[4][13] = {0};
+
+	srand(time(0));
+
+
+
+	return 0;
+}
