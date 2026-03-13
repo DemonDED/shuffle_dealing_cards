5d195d6d5c0aeda0f5d8ca8a92c79fa88950ae0a
DemonDED <DemonDED@inbox.ru>
2026-03-13 14:27:02 +0700

    function deal complete

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char *wFace, const char *wSuit[]);
 
 int main() {
 	const char *suit[4] = {"Hearts", "Diamonds", "Clubs", "Spades"};
@@ -32,3 +33,18 @@ void shuffle(int wDeck[][13]) {
 	}
 }
 
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[]) {
+	int card;
+	int row;
+	int column;
+
+	for (card = 1; card <= 52; card++) {
+		for (row = 0; row <= 3; row++) {
+			for (column = 0; column <= 12; column++) {
+				if (wDeck[row][column] == card) {
+					printf("%5s of %-8s%c", wFace[column], wSuit[row], card % 2 == 0 ? '\n':'\t');
+				}
+			}
+		}
+	}
+}
